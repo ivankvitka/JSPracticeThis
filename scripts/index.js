6f8287b9f7459120fd25eda42a03800fdac1a2d4
Ivan Kvitka <ivan.kvitka@gmail.com>
2018-12-28 12:00:19 +0200

    remove console.log

--- scripts/index.js
+++ scripts/index.js
@@ -2,57 +2,21 @@ var obj = {
   'name': 'someone',
   'number': 20,
 };
-
-function getName() {
-  return this.name;
-}
-
+//task #1
 console.log(getName.call(obj));
 console.log(getName());
 
-function getDoubled() {
-  return this.number * 2;
-}
-
-function getDoubledTrippeled(func) {
-  return func * 3;
-}
-
+//task #2
 console.log(getDoubled.call(obj));
 console.log(getDoubledTrippeled(getDoubled.call(obj)));
 
+//task #3
+var mercedes = new Car('mercedes', '2008', '12345', 'red');
+console.log(mercedes);
+var toyota = new Car('toyota', '2018', '12345', 'white');
+console.log(toyota);
 
-function Car() {
-  this.brand = 'mercedes';
-  this.year = '2018';
-  this.mileage = '1423987';
-  this.color = 'black';
-  this.isMotorStarted = false;
-  this.isMoving = false;
-  this.isContainerFull = false;
-}
-
-var car = new Car();
-console.log(car);
-
-function City(name, population, country) {
-  this.name = name;
-  this.population = population;
-  this.country = country;
-
-  this.getPopulation = function() {
-    return this.population;
-  };
-
-  this.getCityName = function() {
-    return this.name;
-  };
-
-  this.addCitizen = function() {
-    this.population++;
-  }
-}
-
+//task #4
 var kyiv = new City('Kyiv', 3000000, 'Ukraine');
 var newYork = new City('New-York', 12000000);
 
@@ -68,16 +32,7 @@ console.log(newYork.getCityName());
 newYork.addCitizen();
 console.log(newYork.getPopulation());
 
-
-function ask(question, answer, ok, fail) {
-  var result = prompt(question, '');
-  if (result === answer) {
-    ok();
-  } else {
-    fail();
-  }
-}
-
+//task #5
 var user = {
   login: 'Andrew',
   password: '12345',
@@ -114,9 +69,27 @@ user = null;
 user2.checkPassword();
 user2.checkPasswordBind();
 
+//task #6
 (function() {
   var imgArr = [{image: 'car.jpg', alt: 'Car'}, {image: 'apple.jpg', alt: 'Apple'}];
   var currentSlide = 0;
+
+  showNextSlide();
+  showCurrentSlide();
+  showNextSlide();
+  showCurrentSlide();
+  showPreviousSlide();
+  showCurrentSlide();
+  showPreviousSlide();
+  showCurrentSlide();
+  showNthSlide(-1);
+  showCurrentSlide();
+  showNthSlide(-1);
+  showCurrentSlide();
+  insertSlide({image: 'something.jpg', alt: 'Something'}, 5);
+  console.log(imgArr);
+  deleteSlide(2);
+  console.log(imgArr);
 
   function showNextSlide() {
     if (currentSlide === imgArr.length - 1) {
@@ -174,21 +147,89 @@ user2.checkPasswordBind();
       imgArr.splice(position, 1);
     }
   }
-
-  showNextSlide();
-  showCurrentSlide();
-  showNextSlide();
-  showCurrentSlide();
-  showPreviousSlide();
-  showCurrentSlide();
-  showPreviousSlide();
-  showCurrentSlide();
-  showNthSlide(-1);
-  showCurrentSlide();
-  showNthSlide(-1);
-  showCurrentSlide();
-  insertSlide({image: 'something.jpg', alt: 'Something'}, 5);
-  console.log(imgArr);
-  deleteSlide(2);
-  console.log(imgArr);
 }());
+
+function getName() {
+  return this.name;
+}
+
+function getDoubled() {
+  return this.number * 2;
+}
+
+function getDoubledTrippeled(func) {
+  return func * 3;
+}
+
+function Car(brand, year, mileage, color) {
+  this.brand = brand;
+  this.year = year;
+  this.mileage = mileage;
+  this.color = color;
+  this.isMotorStarted = false;
+  this.isMoving = false;
+  this.isContainerFull = false;
+
+  this.fullFuelContainer = function () {
+    this.isContainerFull = true;
+  };
+
+  this.startMotor = function () {
+    if (this.isContainerFull) {
+      this.isMotorStarted = true;
+    } else {
+      console.log("Проще завести, когда есть топливо");
+    }
+  };
+
+  this.stopMotor = function () {
+    this.isMotorStarted = false;
+  };
+
+  this.startMoving = function () {
+    if (this.isMotorStarted) {
+      this.isMoving = true;
+      console.log("Машина " + this.brand + " марки " + this.color + " цвета поехала!")
+    }
+    else {
+      console.log("Включите вначале зажигание");
+    }
+  };
+
+  this.stopMoving = function () {
+    if (!this.isMotorStarted) {
+      console.log("Зажигание и так выключено");
+    } else {
+      this.isMoving = false;
+      console.log("Машинка остановилась");
+    }
+  };
+}
+
+function City(name, population, country) {
+  this.name = name;
+  this.population = population;
+  this.country = country;
+
+  this.getPopulation = function() {
+    return this.population;
+  };
+
+  this.getCityName = function() {
+    return this.name;
+  };
+
+  this.addCitizen = function() {
+    this.population++;
+  }
+}
+
+
+function ask(question, answer, ok, fail) {
+  var result = prompt(question, '');
+  if (result === answer) {
+    ok();
+  } else {
+    fail();
+  }
+}
